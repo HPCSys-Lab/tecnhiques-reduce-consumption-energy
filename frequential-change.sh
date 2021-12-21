39d638da3599dff6bf29e4d8a827d061b9fc7ae0
Reginaldo Junior <reginaldo.junior@vagas.com.br>
2021-12-21 15:39:11 -0300

    Alter governor cpu-freq and add more ensembles to test in moa

--- frequential-change.sh
+++ frequential-change.sh
@@ -22,24 +22,24 @@ cset shield -c 0,1,2,3 -k on --force
 # quantas vezes o programa irá rodar $NUM_RUNS
 for i in $(seq 1 3);
 do
-	echo "Rodando na frequencia 500MHz x 500MHz com a governança userspace"
+	echo "Rodando na frequencia 600MHz x 600MHz com a governança userspace"
 	# setar a frequencia nova nestes cpus
-	cpufreq-set -c 0,1,2,3 -d 500MHz -u 500MHz -g userspace
-	./moa-exec.sh -f 500 -F 500
+	cpufreq-set -c 0,1,2,3 -d 600MHz -u 600MHz -g userspace
+	./moa-exec.sh -f 600 -F 600
 
+	echo "Rodando na frequencia 800MHz x 800MHz com a governança userspace"
+	cpufreq-set -c 0,1,2,3 -d 800MHz -u 800MHz -g userspace
+	./moa-exec.sh -f 800 -F 800
+	
 	echo "Rodando na frequencia 1000MHz x 1000MHz com a governança userspace"
 	cpufreq-set -c 0,1,2,3 -d 1000MHz -u 1000MHz -g userspace
 	./moa-exec.sh -f 1000 -F 1000
-	
-	echo "Rodando na frequencia 2000MHz x 2000MHz com a governança userspace"
-	cpufreq-set -c 0,1,2,3 -d 2000MHz -u 2000MHz -g userspace
-	./moa-exec.sh -f 2000 -F 2000
 
-	echo "Rodando na frequencia 3200MHz x 3200MHz com a governança userspace"
-	cpufreq-set -c 0,1,2,3 -d 3200MHz -u 3200MHz -g userspace
-	./moa-exec.sh -f 3200 -F 3200
+	echo "Rodando na frequencia 1200MHz x 1200MHz com a governança userspace"
+	cpufreq-set -c 0,1,2,3 -d 1200MHz -u 1200MHz -g userspace
+	./moa-exec.sh -f 1200 -F 1200
 done
 
 echo "Retorna padrão do CPU"
 cset shield --reset --force
-cpufreq-set -c 0,1,2,3 -d 500MHz -u 3200MHz
+cpufreq-set -c 0,1,2,3 -d 600MHz -u 3200MHz
