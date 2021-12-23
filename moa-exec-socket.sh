eaae558a0d09cc2ec4c7c53515519d2c9c18045e
Reginaldo Junior <reginaldo.junior@vagas.com.br>
2021-12-23 12:14:53 -0300

    Adiciona o moa-exec-socket.sh

---
+++ moa-exec-socket.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+function Y {
+  #Usage: $0 FILE ALGORITHM RATE
+  Memory=700M
+  echo "file: $1 algorithm: $2 batch_size: $3 rate: $4"
+
+  export MOA_HOME=/home/pi/moa/moa-LAST
+  export RESULT_DIR=/home/pi/moa/ChannelProdb50and250
+  export REMOTE_DIR=/home/gcassales/bases/
+  export EXPER_ORDER_FILE=$RESULT_DIR/exper_order-bsize50and250.log
+
+  declare -a esize=(25)
+  mkdir -p $RESULT_DIR
+  faux=${1##*\/}
+  onlyname=${faux%%.*}
+  bsize=${3}
+  rate=${4}
+  nCores=4
+  date +"%d/%m/%y %T"
+  date +"%d/%m/%y %T" >> $EXPER_ORDER_FILE
+  echo "ssh-${onlyname}-${2##*.}-${bsize}-${rate}" >> ${RESULT_DIR}/ssh-log
+  ssh gcassales@192.168.0.11 java ChannelServer 192.168.0.11 9004 ${REMOTE_DIR}${faux} ${rate} >> ${RESULT_DIR}/ssh-log &
+  sleep 3
+  if [[ $2 == *"MAX"* ]]; then
+    #CHUNK
+    IDENT="timedchunk"
+    echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-${nCores}-${bsize}-${rate}" >> ${EXPER_ORDER_FILE}
+    #echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-${nCores}-${bsize}-${rate}"
+    java -Xshare:off -XX:+UseParallelGC -Xmx$Memory -cp $MOA_HOME/lib/:$MOA_HOME/lib/moa.jar moa.DoTask "ChannelChunksTIMED -l ($2 -s ${esize} -c ${nCores}) -s (ArffFileStream -f $1) -c ${bsize} -e (BasicClassificationPerformanceEvaluator -o -p -r -f) -i -1 -d $RESULT_DIR/dump-${onlyname}-${2##*.}-${esize}-${nCores}-${bsize}-${rate}" > ${RESULT_DIR}/term-${onlyname}-${2##*.}-${esize}-${nCores}-${bsize}-${rate}
+  elif [[ ${2} == *"RUNPER"* ]]; then
+    #PARALLEL
+    IDENT="timedinterleaved"
+    echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-${nCores}-1-${rate}" >> ${EXPER_ORDER_FILE}
+    #echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-${nCores}-1-${rate}"
+    java -Xshare:off -XX:+UseParallelGC -Xmx$Memory -cp $MOA_HOME/lib/:$MOA_HOME/lib/moa.jar moa.DoTask "ChannelTIMED -l ($2 -s ${esize} -c ${nCores}) -s (ArffFileStream -f $1) -e (BasicClassificationPerformanceEvaluator -o -p -r -f) -i -1 -d $RESULT_DIR/dump-${onlyname}-${2##*.}-${esize}-${nCores}-1-${rate}" > ${RESULT_DIR}/term-${onlyname}-${2##*.}-${esize}-${nCores}-1-${rate}
+  else
+    #SEQUENTIAL OR PARALLEL
+    IDENT="timedinterleaved"
+    echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-1-1-${rate}" >> ${EXPER_ORDER_FILE}
+    #echo "$RESULT_DIR/${onlyname}-${2##*.}-${esize}-1-1-${rate}"
+    java -Xshare:off -XX:+UseParallelGC -Xmx$Memory -cp $MOA_HOME/lib/:$MOA_HOME/lib/moa.jar moa.DoTask "ChannelTIMED -l ($2 -s ${esize}) -s (ArffFileStream -f $1) -e (BasicClassificationPerformanceEvaluator -o -p -r -f) -i -1 -d $RESULT_DIR/dump-${onlyname}-${2##*.}-${esize}-1-1-${rate}" > ${RESULT_DIR}/term-${onlyname}-${2##*.}-${esize}-1-1-${rate}
+  fi
+  echo ""
+  date +"%d/%m/%y %T"
+  date +"%d/%m/%y %T" >> $EXPER_ORDER_FILE
+}
+
+function X {
+  #Usage: $0 FILE ID RS RP RC
+  declare -a algs=(
+  "meta.AdaptiveRandomForestSequential" "meta.AdaptiveRandomForestExecutorRUNPER" "meta.AdaptiveRandomForestExecutorMAXChunk"
+  "meta.OzaBag" "meta.OzaBagExecutorRUNPER" "meta.OzaBagExecutorMAXChunk"
+  "meta.OzaBagAdwin" "meta.OzaBagAdwinExecutorRUNPER" "meta.OzaBagAdwinExecutorMAXChunk"
+  "meta.LeveragingBag" "meta.LBagExecutorRUNPER" "meta.LBagExecutorMAXChunk"
+  "meta.OzaBagASHT" "meta.OzaBagASHTExecutorRUNPER" "meta.OzaBagASHTExecutorMAXChunk"
+  "meta.StreamingRandomPatches" "meta.StreamingRandomPatchesExecutorRUNPER" "meta.StreamingRandomPatchesExecutorMAXChunk"
+  )
+  if [[ $2 == *"ARF"* ]]; then
+    ID=0
+  elif [[ $2 == "OBag" ]]; then
+    ID=3
+  elif [[ $2 == "OBagAd" ]]; then
+    ID=6
+  elif [[ $2 == "LBag" ]]; then
+    ID=9
+  elif [[ $2 == "OBagASHT" ]]; then
+    ID=12
+  elif [[ $2 == "SRP" ]]; then
+    ID=15
+  fi
+  #Y $1 ${algs[${ID}]} $3 $4
+  #Y $1 ${algs[$(( ID+1 ))]} $3 $5
+  Y $1 ${algs[$(( ID+2 ))]} $3 $6
+}
+
+## Adiciona aqui a geração dinamica do notebook
+
+date + "%d/%m/%y %T" >> $EXPER_ORDER_FILE
