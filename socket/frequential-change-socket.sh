0c72f65d7f87152495571f2759a8fb869c65b662
Reginaldo Junior <76regi@gmail.com>
2022-03-04 13:38:17 -0300

    Update rules to new experiments

--- socket/frequential-change-socket.sh
+++ socket/frequential-change-socket.sh
@@ -10,46 +10,59 @@ sudo cset shield --reset --force
 # setar os novos cpus disponiveis
 echo "Criando novos CPUs virtuais"
 
-### this code run only in machines with docker installed
-# echo "stop all dockers"
-# docker stop $(docker ps)
-# echo "set docker cpuset empty"
-# echo > /sys/fs/cgroup/cpuset/docker/cpuset.cpus
-# echo "starting cpu container"
+function run_only_mini_batching {
+    echo "Resetando todos os CPUs virtuais"
+    sudo cset shield --reset --force
 
-sudo cset shield -c 0 -k on --force
+	echo "Rodando na frequencia padrão"
+	sudo cpufreq-set -c 0,1,2,3 -d 600MHz -u 1200MHz -g performance
+	./moa-exec-minibatching.sh -f 600 -F 1200
+}
 
-echo "Rodando na frequencia 600MHz x 600MHz com a governança userspace"
-# setar a frequencia nova nestes cpus
-sudo cpufreq-set -c 0 -d 600MHz -u 600MHz -g userspace
-./moa-exec-socket-600.sh -c 0 -f 600 -F 600
+function run_only_mini_batching_to_xu3em {
+    echo "Resetando todos os CPUs virtuais"
+    sudo cset shield --reset --force
 
-echo "Rodando na frequencia 1200MHz x 1200MHz com a governança userspace"
-sudo cpufreq-set -c 0 -d 1200MHz -u 1200MHz -g userspace
-./moa-exec-socket-1200.sh -c 0 -f 1200 -F 1200
+	echo "Rodando na frequencia 600x600"
+	sudo cpufreq-set -c 0,1,2,3 -d 600MHz -u 600MHz -g powersave
+	./moa-exec-minibatching.sh -f 600 -F 600
 
-sudo cset shield -c 0,1 -k on --force
+	echo "Rodando na frequencia 1200x1200"
+	sudo cpufreq-set -c 0,1,2,3 -d 1200MHz -u 1200MHz -g powersave
+	./moa-exec-minibatching.sh -f 1200 -F 1200
 
-echo "Rodando na frequencia 600MHz x 600MHz com a governança userspace"
-# setar a frequencia nova nestes cpus
-sudo cpufreq-set -c 0,1 -d 600MHz -u 600MHz -g userspace
-./moa-exec-socket-600.sh -c 2 -f 600 -F 600
+}
 
-echo "Rodando na frequencia 1200MHz x 1200MHz com a governança userspace"
-sudo cpufreq-set -c 0,1 -d 1200MHz -u 1200MHz -g userspace
-./moa-exec-socket-1200.sh -c 2 -f 1200 -F 1200
+function run_xue3m_without_mini_batching_paralel_2_cpu {
+    sudo cset shield -c 0,1 -k on --force
 
-sudo cset shield -c 0,1,2 -k on --force
+    echo "Rodando na frequencia 600MHz x 600MHz com a governança powersave"
+    # setar a frequencia nova nestes cpus
+    sudo cpufreq-set -c 0,1 -d 600MHz -u 600MHz -g powersave
+    ./moa-exec-xue3m-600.sh -c 2 -f 600 -F 600
 
-echo "Rodando na frequencia 600MHz x 600MHz com a governança userspace"
-# setar a frequencia nova nestes cpus
-sudo cpufreq-set -c 0,1,2 -d 600MHz -u 600MHz -g userspace
-./moa-exec-socket-600.sh -c 3 -f 600 -F 600
+    echo "Rodando na frequencia 1200MHz x 1200MHz com a governança powersave"
+    sudo cpufreq-set -c 0,1 -d 1200MHz -u 1200MHz -g powersave
+    ./moa-exec-xue3m-1200.sh -c 2 -f 1200 -F 1200
+}
 
-echo "Rodando na frequencia 1200MHz x 1200MHz com a governança userspace"
-sudo cpufreq-set -c 0,1,2 -d 1200MHz -u 1200MHz -g userspace
-./moa-exec-socket-1200.sh -c 3 -f 1200 -F 1200
+function run_xue3m_without_mini_batching_paralel_3_cpu {
+    sudo cset shield -c 0,1,2 -k on --force
 
-echo "Retorna padrão do CPU"
-sudo cset shield --reset --force
-sudo cpufreq-set -c 0,1,2,3 -d 600MHz -u 1200MHz
+    echo "Rodando na frequencia 600MHz x 600MHz com a governança powersave"
+    # setar a frequencia nova nestes cpus
+    sudo cpufreq-set -c 0,1,2 -d 600MHz -u 600MHz -g powersave
+    ./moa-exec-xue3m-600.sh -c 3 -f 600 -F 600
+
+    echo "Rodando na frequencia 1200MHz x 1200MHz com a governança powersave"
+    sudo cpufreq-set -c 0,1,2 -d 1200MHz -u 1200MHz -g powersave
+    ./moa-exec-xue3m-1200.sh -c 3 -f 1200 -F 1200
+}
+
+function reset_cpus_and_freq {
+    echo "Retorna padrão do CPU"
+    sudo cset shield --reset --force
+    sudo cpufreq-set -c 0,1,2,3 -d 600MHz -u 1200MHz
+}
+
+run_only_mini_batching
